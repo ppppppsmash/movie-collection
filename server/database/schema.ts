d9b9b86a80c266fb70d8734a72d2924e809d4269
ppppppsmash <pppppp.work@gmail.com>
2024-04-29 21:26:20 +0900

    chore: drizzle init

---
+++ server/database/schema.ts
@@ -0,0 +1,7 @@
+// https://hub.nuxt.com/docs/recipes/drizzle
+import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
+
+export const users = sqliteTable('users', {
+  id: integer('id').primaryKey({ autoIncrement: true }),
+  name: text('name').notNull()
+})
