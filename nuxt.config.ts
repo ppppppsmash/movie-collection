0a7d647dd539cdfde3945234d0f27fc970803db3
ppppppsmash <pppppp.work@gmail.com>
2024-05-02 21:54:51 +0900

    chore: supabase for nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
   },
   modules: [
     '@unocss/nuxt',
-    '@vueuse/nuxt'
+    '@vueuse/nuxt',
+    '@nuxtjs/supabase'
   ]
 })
--- server/api/movies/[...].ts
+++ server/api/movies/[...].ts
@@ -11,5 +11,5 @@ export default async (event: any) => {
       }
     })
 
-    return result
+  return result
 }
